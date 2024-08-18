099bdd9467e26c93364dcfced77cc3db63fef09d
sujan pal <sujanpal79@gmail.com>
2024-08-19 01:11:02 +0530

    Update ImageSlider component

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 
-const ImageSlider = () => {
+
+const ImageSlider = ({apiEndpoit}) => {
+    const [images, setImage] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0)
+
+    useEffect(()=>{
+        const fetchImages = async() =>{
+            try{
+                const response = await fetch(apiEndpoit);
+                const data = await response.json();
+                setImage(data)
+            }catch(error){
+                console.error('Error fetching images:', error)
+            }
+        };
+        fetchImages();
+    },[apiEndpoit])
+
+    const nextSlide = ()=>{
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }
+    const prevSlide = () =>{
+        setCurrentIndex(()=>
+            prevIndex === 0 ? images.length - 1:prevIndex - 1
+        );
+    };
+    const goToSlide = (index) =>{
+        setCurrentIndex(index);
+    }
+    if(images.length === 0 ){
+        return <div>Loading...</div>;
+    }
+  return (
+    <div>SliderContent</div>
+  )
   return (
     <div>ImageSlider</div>
   )
 }
 
-export default ImageSlider
+export default ImageSlider;
--- src/components/SliderContent.js
+++ src/components/SliderContent.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const SliderContent = () => {
-  return (
-    <div>SliderContent</div>
-  )
+const SliderContent = ({currentIndex, images}) => {
+    return(
+        <div className='slider-content' style={{transform:`translateX(-${currentIndex*100}%)`}}>
+            {images.map((image,index) => (
+                <img key={index} src={image.url} alt={`Slide ${index}`} className='slide'/>
+            ))}
+        </div>
+    )
 }
 
-export default SliderContent
+export default SliderContent;
